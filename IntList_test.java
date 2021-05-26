a482179f15f196f47a3c52b9651e5667c5da0230
duliwencynthia <49981557+duliwencynthia@users.noreply.github.com>
2021-05-27 01:10:51 +0800

    Update IntList_test.java

--- IntList_test.java
+++ IntList_test.java
@@ -2,12 +2,12 @@ package lab2;
 
 public class IntList_test {
 	public static void main(String[] args) {
-		intlist myList = new intlist(0, null);
-	    myList.rest = new intlist(1, null);
-	    myList.rest.rest = new intlist(2, null);
-	    myList.rest.rest.rest = new intlist(3, null);
+	    IntList myList = new IntList(0, null);
+	    myList.rest = new IntList(1, null);
+	    myList.rest.rest = new IntList(2, null);
+	    myList.rest.rest.rest = new IntList(3, null);
 	    
-	    intlist squarelist=myList.squarelistinteractive(myList.rest);
+	    IntList squarelist=myList.squarelistinteractive(myList.rest);
 	    while (squarelist!=null) {
 	    	System.out.println(squarelist.first);
 	    	squarelist=squarelist.rest;
